d2fb71febf7ef166fe8c51a70d61ec0dbd93eff2
Martin <mycha0@hotmail.com>
2017-06-06 19:43:02 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const { Subject } = require('rxjs/Subject')
+
+
+module.exports = genFn => (...args) => ({
+  subscribe: (onNext, onError, onComplete) => {
+    const subject$ = new Subject()
+    subject$.subscribe(onNext, onError, onComplete)
+    if ( typeof genFn !== 'function' || !isGenerator(genFn()) ) {
+      return subject$.error(
+        new Error(`Expecting generator function, but instead got "${JSON.stringify(genFn) || String(genFn)}"`)
+      )
+    }
+    next( genFn(...args), subject$ )
+  }
+})
+
+
+const next = (gen, subject$, err = undefined, data = undefined) => {
+  try {
+    const { done, value } = err ? { done: true, value: null } : gen.next(data)
+    if (done) {
+      subject$.complete()
+      subject$.dispose()
+    }
+
+    if ( isPromise(value) ) {
+      value
+        .then(v => {
+          subject$.next(v)
+          setImmediate(() => next(gen, subject$, null, v))
+        })
+        .catch(e => subject$.error(e))
+    } else {
+      subject$.next(value)
+      setImmediate(() => next(gen, subject$, err, data))
+    }
+  } catch (e) {
+    subject$.error(e)
+  }
+}
+
+
+const isGenerator = obj =>
+  Boolean(obj) &&
+  typeof obj.next === 'function' &&
+  typeof obj.return === 'function' &&
+  typeof obj.throw === 'function'
+
+
+const isPromise = obj =>
+  Boolean(obj) &&
+  typeof obj.then === 'function' &&
+  typeof obj.catch === 'function'
---
+++ tests/index-test.js
@@ -0,0 +1,49 @@
+'use strict'
+
+import rxgen from '../'
+import test from 'ava'
+
+
+test('should give error when passing invalid param to rxgen', t => {
+  const params = [ undefined, null, 1, '1', [], {}, () => {}, Promise.resolve(1) ]
+  t.plan(params.length)
+  params.forEach(param =>
+    rxgen(param)()
+      .subscribe(null, e => t.true(e instanceof Error))
+  )
+})
+
+
+test('should give error when generator function yield error', t => {
+  t.plan(1)
+  const fn = rxgen(function* (n) {
+    while (n > 0) {
+      yield new Promise((resolve, reject) =>
+        n % 6 ? resolve(n) : reject(n)
+      )
+      n--
+    }
+  })
+
+  return new Promise((_, reject) =>
+    fn(10).subscribe(null, e => reject(e))
+  )
+  .catch(e => t.true(e === 6))
+})
+
+
+test('shoud finish executing generator function successfully', t => {
+  t.plan(1)
+  const fn = rxgen(function* (n) {
+    while (n > 0) {
+      yield new Promise(resolve => resolve(n))
+      n--
+    }
+  })
+
+  return new Promise(resolve =>
+    fn(3).subscribe(null, null, () => resolve())
+  )
+  .then(_ => t.pass())
+})
+
